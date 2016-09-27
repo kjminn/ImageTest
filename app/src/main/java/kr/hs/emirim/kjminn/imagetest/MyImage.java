a583eca372e8cbe7de7e1b24023e01b35619b9a8
Kyoungju Minn <kjminn2001@gmail.com>
2016-09-27 20:40:19 +0900

    #4 Event Programming

--- app/src/main/java/kr/hs/emirim/kjminn/imagetest/MyImage.java
+++ app/src/main/java/kr/hs/emirim/kjminn/imagetest/MyImage.java
@@ -10,27 +10,42 @@ import android.view.View;
  * Created by appcreator00 on 2016. 9. 26..
  */
 public class MyImage extends View{
+    int choose=MainActivity.ORIGINAL;
+    Bitmap picture;
+
     MyImage(Context context){
         super(context);
+        picture= BitmapFactory.decodeResource(getResources(), R.drawable.puppy);
+    }
+
+    public void setChoose(int choose) {
+        this.choose = choose;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        Bitmap picture= BitmapFactory.decodeResource(getResources(), R.drawable.puppy);
+
         float cx=getWidth()/2.0f;
         float cy=getWidth()/2.0f;
         float x=(getWidth()-picture.getWidth())/2.0f;
         float y=(getHeight()-picture.getHeight())/2.0f;
-//            회전
-//            canvas.rotate(180,cx, cy);
-//            이동
-//            canvas.translate(-150,200);
-//            크기
-//            canvas.scale(1.5f,1.5f,cx,cy);
-//            비틀기
-        canvas.skew(0.4f,0.4f);
+        switch (choose){
+            case MainActivity.ROTATE:
+                canvas.rotate(180,cx, cy);
+                break;
+            case MainActivity.TRANSLATE:
+                canvas.translate(-150,200);
+                break;
+            case MainActivity.SCALE:
+                canvas.scale(1.5f,1.5f,cx,cy);
+                break;
+            case MainActivity.SKEW:
+                canvas.skew(0.4f,0.4f);
+                break;
+        }
         canvas.drawBitmap(picture,x,y,null);
+//        picture.recycle();
     }
 }
